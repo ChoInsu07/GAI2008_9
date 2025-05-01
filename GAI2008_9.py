dde56235c3edc0e4466af534adeb2327fbd33b46
ChoInsu07 <iwantsleep1007@yonsei.ac.kr>
2025-05-01 11:35:40 +0900

    add neg & mul function

--- GAI2008_9.py
+++ GAI2008_9.py
@@ -5,20 +5,14 @@ class Complex:
 
     def __neg__(self):
         return Complex(-self.a,-self.b)
-
-    def __add__(self, operand):
-        new_a = self.a + operand.a
-        new_b = self.b + operand.b
-        return Complex(new_a,new_b)
+    '''
+    def __add__():
     
-    def __sub__(self, operand):
-        new_a = self.a - operand.a
-        new_b = self.b - operand.b
-        return Complex(new_a,new_b)
-    
+    def __sub__():
+    '''
     def __mul__(self, operand):
-        new_a = self.a * operand.a
-        new_b = self.b * operand.b
+        new_a = self.a * operand.a - self.b * operand.b
+        new_b = self.a * operand.b + self.b * operand.a
         return Complex(new_a,new_b)
     
     def __str__(self):
@@ -31,6 +25,4 @@ if __name__ == '__main__':
     A = Complex(1,2)
     B = Complex(4,2)
     print(-A)
-    print(A+B)
-    print(A-B)
     print(A*B)
